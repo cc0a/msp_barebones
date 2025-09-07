13fc3274e76a49f417e34ee5f50d71854812975a
John Smith <jmckeeco84@gmail.com>
2025-09-06 20:43:26 -0600

    additional host pools added

--- environments/test/test.tf
+++ environments/test/test.tf
@@ -1,10 +1,16 @@
 # environments/test.tf
 
+# ============================
+# Resource Group
+# ============================
 resource "azurerm_resource_group" "test_rg" {
   name     = "test-rg"
   location = var.location
 }
 
+# ============================
+# Networking
+# ============================
 resource "azurerm_virtual_network" "test_vnet" {
   name                = "test-vnet"
   location            = var.location
@@ -19,8 +25,11 @@ resource "azurerm_subnet" "test_subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "test_nic" {
-  name                = "test-nic"
+# ============================
+# NICs for VMs
+# ============================
+resource "azurerm_network_interface" "nic_nginx" {
+  name                = "nic-nginx"
   location            = var.location
   resource_group_name = azurerm_resource_group.test_rg.name
 
@@ -31,15 +40,54 @@ resource "azurerm_network_interface" "test_nic" {
   }
 }
 
+resource "azurerm_network_interface" "nic_sql" {
+  name                = "nic-sql"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.test_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "nic_memcache" {
+  name                = "nic-memcache"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.test_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "nic_win2022" {
+  name                = "nic-win2022"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.test_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# ============================
+# Linux VMs
+# ============================
 resource "azurerm_linux_virtual_machine" "test_vm_nginx" {
-  name                = "test-vm"
+  name                = "test-vm-nginx"
   location            = var.location
   resource_group_name = azurerm_resource_group.test_rg.name
   size                = "Standard_B1s"
   admin_username      = "azureuser"
 
   network_interface_ids = [
-    azurerm_network_interface.test_nic.id
+    azurerm_network_interface.nic_nginx.id
   ]
 
   admin_ssh_key {
@@ -58,16 +106,17 @@ resource "azurerm_linux_virtual_machine" "test_vm_nginx" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+}
 
-  resource "azurerm_linux_virtual_machine" "test_vm_mservice01_sql" {
-  name                = "test-vm"
+resource "azurerm_linux_virtual_machine" "test_vm_sql" {
+  name                = "test-vm-sql"
   location            = var.location
   resource_group_name = azurerm_resource_group.test_rg.name
   size                = "Standard_B1s"
   admin_username      = "azureuser"
 
   network_interface_ids = [
-    azurerm_network_interface.test_nic.id
+    azurerm_network_interface.nic_sql.id
   ]
 
   admin_ssh_key {
@@ -86,16 +135,17 @@ resource "azurerm_linux_virtual_machine" "test_vm_nginx" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+}
 
-    resource "azurerm_linux_virtual_machine" "test_vm_mservice01_memcache" {
-  name                = "test-vm"
+resource "azurerm_linux_virtual_machine" "test_vm_memcache" {
+  name                = "test-vm-memcache"
   location            = var.location
   resource_group_name = azurerm_resource_group.test_rg.name
   size                = "Standard_B1s"
   admin_username      = "azureuser"
 
   network_interface_ids = [
-    azurerm_network_interface.test_nic.id
+    azurerm_network_interface.nic_memcache.id
   ]
 
   admin_ssh_key {
@@ -114,9 +164,13 @@ resource "azurerm_linux_virtual_machine" "test_vm_nginx" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+}
 
-  resource "azurerm_windows_virtual_machine" "test_vm_win2022_file_server" {
-  name                = "test-vm"
+# ============================
+# Windows VM
+# ============================
+resource "azurerm_windows_virtual_machine" "test_vm_win2022_file_server" {
+  name                = "test-vm-win2022"
   location            = var.location
   resource_group_name = azurerm_resource_group.test_rg.name
   size                = "Standard_B1s"
@@ -124,7 +178,7 @@ resource "azurerm_linux_virtual_machine" "test_vm_nginx" {
   admin_password      = "P@ssw0rd1234!"   # ⚠️ Replace with a secure secret
 
   network_interface_ids = [
-    azurerm_network_interface.test_nic.id
+    azurerm_network_interface.nic_win2022.id
   ]
 
   os_disk {
@@ -138,16 +192,44 @@ resource "azurerm_linux_virtual_machine" "test_vm_nginx" {
     sku       = "2022-Datacenter"
     version   = "latest"
   }
-  resource "azurerm_virtual_desktop_host_pool" "avd_hostpool" {
+}
+
+# ============================
+# Azure Virtual Desktop Host Pools
+# ============================
+resource "azurerm_virtual_desktop_host_pool_01" "avd_hostpool" {
   name                = "avd-hostpool-01"
   location            = var.location
   resource_group_name = azurerm_resource_group.test_rg.name
 
-  type                = "Pooled"         # or "Personal"
-  load_balancer_type  = "BreadthFirst"   # options: BreadthFirst, DepthFirst, Persistent
-  preferred_app_group_type = "Desktop"
+  type                      = "Pooled"
+  load_balancer_type        = "BreadthFirst"
+  preferred_app_group_type  = "Desktop"
 
   maximum_sessions_allowed = 10
   friendly_name            = "Test AVD Host Pool"
-  }
+
+  resource "azurerm_virtual_desktop_host_pool_02" "avd_hostpool" {
+  name                = "avd-hostpool-01"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+
+  type                      = "Pooled"
+  load_balancer_type        = "BreadthFirst"
+  preferred_app_group_type  = "Desktop"
+
+  maximum_sessions_allowed = 10
+  friendly_name            = "Test AVD Host Pool"
+
+  resource "azurerm_virtual_desktop_host_pool_03" "avd_hostpool" {
+  name                = "avd-hostpool-01"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+
+  type                      = "Pooled"
+  load_balancer_type        = "BreadthFirst"
+  preferred_app_group_type  = "Desktop"
+
+  maximum_sessions_allowed = 10
+  friendly_name            = "Test AVD Host Pool"
 }
